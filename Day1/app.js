ebe43a23b98569241fb2667b29dd8c135c67ac11
ayushjaiswal97 <ayushjaiswal9099@gmail.com>
2025-07-21 16:58:30 +0530

    Refactor app.js to include multiple child elements and update index.html for better structure

--- Day1/app.js
+++ Day1/app.js
@@ -1,12 +1,16 @@
-const parent = React.createElement(
-    "div",
-    {id: "parent"},
-    React.createElement(
-        "div",
-        {id: "child"},
-        React.createElement('h1', {}, "I'm a h1 tag")
-    )
-);
+const parent = React.createElement("div", {id: "parent" }, [
+    React.createElement("div", {id: "child"}, [
+      React.createElement('h1', {}, "I'm a h1 tag"),
+      React.createElement('h2', {}, "I'm a h2 tag")
+    ]),
+    React.createElement("div", {id: "child"}, [
+      React.createElement('h1', {}, "I'm a h1 tag"),
+      React.createElement('h2', {}, "I'm a h2 tag")
+    ]),
+]);
+
+
+// JSX
 
 const heading = React.createElement(
     "h1",
