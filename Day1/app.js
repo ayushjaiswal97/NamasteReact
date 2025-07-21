d6bbe0a21555e9403b92c6f32b87238c0e73168b
ayushjaiswal97 <ayushjaiswal9099@gmail.com>
2025-07-21 16:27:52 +0530

    Refactor app.js to create a parent div and render it, updating index.html to include app.js

--- Day1/app.js
+++ Day1/app.js
@@ -1,11 +1,21 @@
+const parent = React.createElement(
+    "div",
+    {id: "parent"},
+    React.createElement(
+        "div",
+        {id: "child"},
+        React.createElement('h1', {}, "I'm a h1 tag")
+    )
+);
+
 const heading = React.createElement(
     "h1",
     {id: "heading", xyz: "abc"}, 
     "Hello World from React!"
 );
 
-console.log(heading);
+console.log(parent); // objects
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading); 
+root.render(parent); 
