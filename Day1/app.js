a5deb41efbfc27e83cd59c5df16251c569ff232f
ayushjaiswal97 <ayushjaiswal9099@gmail.com>
2025-07-21 15:43:13 +0530

    Add CSS styling for heading and update app.js to include attributes

--- Day1/app.js
+++ Day1/app.js
@@ -1,5 +1,6 @@
-const heading = React.createElement("h1", {}, "Hello World from React!");
+const heading = React.createElement("h1", {id: "heading", xyz: "abc"}, "Hello World from React!");
+console.log(heading);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading);
+root.render(heading); 
